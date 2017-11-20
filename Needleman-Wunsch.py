44f380623b1eeef6cfd490f447925c50c2825d17
YaoXinZhi <391166792@qq.com>
2017-11-20 21:55:36 +0800

    这是第一次提交

---
+++ Needleman-Wunsch.py
@@ -0,0 +1,112 @@
+
+# file NEddle
+# /usr/bin/python
+# coding:utf-8
+
+from numpy import *
+import numpy as np
+import sys
+import getopt
+
+def compare(str1,str2):
+    if (str1 == str2 ):
+        return match_score
+    else:
+        return mismatch_score
+    
+def loss_function(matrix,i,j):
+    #i == j == 1
+    a = matrix[i-1,j] + compare(Seq1[i-1],'-')
+    b = matrix[i,j-1] + compare(Seq2[j-1],'-')
+    c = matrix[i-1,j-1] + compare(Seq1[i-1],Seq2[j-1])
+    matrix[i,j] = max(a,b,c)
+    return matrix
+
+def generate_matrix(file1,file2):
+
+    x=len(file1)+1
+    y=len(file2)+1
+    distance_matrix = mat(zeros((x,y)))
+        
+    for i in range(1,x):
+        distance_matrix[i,0] = distance_matrix[i-1,0]-1    
+    for j in range(1,y):
+        distance_matrix[0,j] = distance_matrix[0,j-1]-1
+           
+    for i in range(1,x):
+        for j in range(1,y):
+            distance_matrix = loss_function(distance_matrix,i,j)
+    return distance_matrix
+
+def print_result(result):
+
+    row , col = shape(result)
+   
+    print()
+    print("        ", end = '')
+    for i in Seq2:
+        print(i, end='\t')
+    print()
+    
+    for i in range(0, row):
+        if (i > 0):
+            print(Seq1[i-1], end = '\t')
+        else:
+            print("  ", end = '')
+    
+        for j in range(0, col):
+            print('%d' % result[i,j], end = '\t')
+        print()
+    
+def get_string(file):
+    with open(file,'r') as f:
+        for line in f:
+            l = line.replace('\n','')
+            if '>' in l:
+                pass
+            else:
+                string = l
+    return string
+
+def usage():
+    print ('please read the file of Readme.txt and to be good lucky !!')
+
+def main():
+    Distance_matrix = generate_matrix(Seq1,Seq2)
+    print_result(Distance_matrix)
+
+if __name__ == '__main__':
+    opts,args = getopt.getopt(sys.argv[1:],'h',['match=','mismatch=','file1=','file2='])
+    InputFile1 = '/home/yaoxinzhi/桌面/Bioinformatics/Needleman-Wunsch/Sequence/Seq1.fasta'
+    InputFile2 = '/home/yaoxinzhi/桌面/Bioinformatics/Needleman-Wunsch/Sequence/Seq2.fasta'
+    match_score = 1
+    mismatch_score = -1
+    
+    for op,value in opts:
+        if op == '--file1':
+            print (value+'111')
+            InputFile1 = value
+            
+        elif op == '--file2':
+            InputFile2 = value
+            print (value)
+        elif op == '-o':
+            OutputFile = value
+        elif op == '-h':
+            usage()
+            sys.exit()
+        elif op == '--match':
+            match_score = float(value)
+        elif op == '--mismatch':
+            mismatch_score = float(value)
+        
+    Seq1=get_string(InputFile1)
+    Seq2=get_string(InputFile2)
+    print ('matrix shape: '+str(len(Seq1))+'*'+str(len(Seq2)))
+    main()
+    
+
+    
+    
+        
+    
